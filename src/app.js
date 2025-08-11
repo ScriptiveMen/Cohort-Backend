a898be83a86db73a38a73e51bf58df591d0b1ceb
ScriptiveMen <i.satyakumarram@gmail.com>
2025-08-11 20:21:39 +0530

    Refactor project structure: remove authentication routes and database connection, update dependencies, and implement EJS for rendering views.

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 const express = require("express");
-const authRoutes = require("./routes/auth.routes");
-const cookieParser = require("cookie-parser");
-
+const indexRoutes = require("./routes/index.routes");
 const app = express();
-app.use(express.json());
-app.use(cookieParser());
 
-app.use("/auth", authRoutes);
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/", indexRoutes);
 
 module.exports = app;
